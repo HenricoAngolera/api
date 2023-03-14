9c5770f42fc8232453c6e44423c1ef9abc680e8c
henrico <henrico.an@gmail.com>
2023-03-14 18:24:36 -0300

    nodemon

--- src/server.js
+++ src/server.js
@@ -8,13 +8,13 @@ app.get('/message/:id/:user', (request, response) => {
   const { id, user } = request.params
 
   response.send(`
-    Message ID: ${id}. 
-    To the user: ${user}.`
-  ) 
+    Message ID: ${id}.
+    User name: ${user}.
+  `)
 })
 
 app.get('/users', (request, response) => {
-  const { page, limit } = request.query;
+  const { page, limit } = request.query
 
   response.send(`Page: ${page}. Show: ${limit}`)
 })
