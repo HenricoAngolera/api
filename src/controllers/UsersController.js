fc75a717a2f3c8d43a8618bb3de7cc0ea5571fcd
henrico <henrico.an@gmail.com>
2023-03-18 20:05:24 -0300

    HTTP Code, created

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,7 +2,8 @@ class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
 
-    response.json({ name, email, password })
+    response.status(201).json({ name, email, password })
+    // Success code, created
   }
 }
 
