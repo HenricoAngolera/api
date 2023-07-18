ae37381461cd3cc75405574592d3d1e52ef256a7
henrico <henrico.an@gmail.com>
2023-07-17 21:16:15 -0300

    testing email in use

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,5 +1,6 @@
 const UserCreateService = require("./UserCreateService")
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError")
 
 describe("UserCreateService", () => {
   it("user should be create", async () => {
@@ -16,14 +17,25 @@ describe("UserCreateService", () => {
     expect(userCreated).toHaveProperty("id");
   })
 
-  it("another test", () => {
-    expect(1).toBe(1)
+  it("user should not be created with existing email", async () => {
+    const user1 = {
+      name: "user test 1",
+      email: "user@test.com",
+      password: "123"
+    };
+
+    const user2 = {
+      name: "user test 2",
+      email: "user@test.com",
+      password: "456"
+    };
+
+    const userRepository = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepository);
+
+    await userCreateService.execute(user1);
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este email já está em uso."))
+
   })
 
-})
-
-describe("NotesController", () => {
-  it("create notes", () => {
-    expect(1).toBe(2)
-  })
 })
